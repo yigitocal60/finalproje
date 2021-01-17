800168be5bf74178cb4d88fe091acc1a194da290
yigitocal60 <18061268@stu.omu.edu.tr>
2021-01-17 20:32:42 +0300

    githuba proje gönderiyorum

---
+++ app/src/main/java/my/foodon/app/DeliveryFoodPanel_BottomNavigation.java
@@ -0,0 +1,58 @@
+package my.foodon.app;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import my.foodon.app.deliveryFoodPanel.DeliveryPendingOrderFragment;
+import my.foodon.app.deliveryFoodPanel.DeliveryShipOrderFragment;
+
+public class DeliveryFoodPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery_food_panel__bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.delivery_bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+        String name = getIntent().getStringExtra( "PAGE");
+        if(name!=null) {
+            if (name.equalsIgnoreCase("DeliveryOrderpage")) {
+                loaddeliveryfragment(new DeliveryPendingOrderFragment());
+            }
+        }else{
+            loaddeliveryfragment(new DeliveryPendingOrderFragment());
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.shiporders:
+                fragment=new DeliveryShipOrderFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.pendingorders:
+                fragment=new DeliveryPendingOrderFragment();
+                break;
+        }
+        return loaddeliveryfragment(fragment);
+    }
+
+    private boolean loaddeliveryfragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ app/src/main/java/my/foodon/app/Loginphone.java
@@ -0,0 +1,63 @@
+package my.foodon.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Loginphone extends AppCompatActivity {
+
+    EditText num;
+    Button sendotp,signinemail;
+    TextView signup;
+    FirebaseAuth Fauth;
+    String number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginphone);
+
+        num = (EditText)findViewById(R.id.number);
+        sendotp = (Button)findViewById(R.id.otp);
+        signinemail=(Button)findViewById(R.id.btnEmail);
+        signup = (TextView)findViewById(R.id.acsignup);
+
+        Fauth = FirebaseAuth.getInstance();
+
+        sendotp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                number=num.getText().toString().trim();
+                Intent b = new Intent(Loginphone.this,sendotp.class);
+
+                String Phonenum = new String();
+                b.putExtra("Phonenumber",Phonenum);
+                startActivity(b);
+                finish();
+
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Loginphone.this,Registration.class));
+                finish();
+            }
+        });
+        signinemail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Loginphone.this,Login.class));
+                finish();
+            }
+        });
+    }
+}
